866aa97e8821764ec70fa17037129397f240f745
karim kamil <kkamil@eg.ibm.com>
2019-08-10 15:36:52 +0200

    ng for for clients

--- src/app/Pages/clients/clients.page.ts
+++ src/app/Pages/clients/clients.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ClientsService } from "../../Services/Clients/clients.service";
+import { Client } from "../../Data-Model/Clients/client";
 
 @Component({
-  selector: 'app-clients',
-  templateUrl: './clients.page.html',
-  styleUrls: ['./clients.page.scss'],
+  selector: "app-clients",
+  templateUrl: "./clients.page.html",
+  styleUrls: ["./clients.page.scss"]
 })
 export class ClientsPage implements OnInit {
-  panelOpenState = false
-  constructor() { }
+  panelOpenState = false;
+  CLients: Client[];
+  constructor(private clientService: ClientsService) {}
 
   ngOnInit() {
+    this.clientService.getAll().subscribe(data => {
+      this.CLients = data.map(client => new Client(client));
+   console.log(this.CLients);
+   
+    });
   }
-
 }
